0755c09134e8f55e9e8b75d0df9c61d964f7fb11
Vladimir <vshtytser@gmail.com>
2023-04-02 16:02:53 -0400

    feat: Add duplicate function to extend list with a copy of itself

---
+++ 1_Programming_basics/2_Python_lists/4_duplicate.py
@@ -0,0 +1,16 @@
+def duplicate(list):
+    """
+    Extends the input list with a copy of itself, effectively duplicating its contents.
+
+    Args:
+        list: A list of elements to be duplicated.
+
+    Returns:
+        None. The input list is modified in place.
+
+    Examples:
+        items = [1, 2]
+        duplicate(items)  # nothing is returned
+        print(items)  # => [1, 2, 1, 2]
+    """
+    list.extend(list)
