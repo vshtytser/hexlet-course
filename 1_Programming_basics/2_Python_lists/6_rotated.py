56ec92918eb1e2807eb6c3bfe115b92c19db5af6
Vladimir <vshtytser@gmail.com>
2023-04-05 15:47:27 -0400

    Add `rotated_left` and `rotated_right` functions, which  rotate a sequence of elements one position to the left and right, respectively

---
+++ 1_Programming_basics/2_Python_lists/6_rotated.py
@@ -0,0 +1,30 @@
+def rotated_left(item):
+    """
+    Rotate the elements of a sequence one position to the left.
+
+    Args:
+        item (sequence): A sequence of elements to be rotated.
+
+    Returns:
+        sequence: The input sequence with the first element moved to the end.
+
+    Example:
+        # rotated_left("ABCD") # "BCDA"
+    """
+    return item[1:] + item[0:1]
+
+
+def rotated_right(item):
+    """
+    Rotate the elements of a sequence one position to the right.
+
+    Args:
+        item (sequence): A sequence of elements to be rotated.
+
+    Returns:
+        sequence: The input sequence with the last element moved to the beginning.
+
+    Example:
+        rotated_right([1, 2, 3, 4])  # [4, 1, 2, 3]
+    """
+    return item[-1:] + item[0:-1]
